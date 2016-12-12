937326a6b93d110021e6cff0ce931d4cd6c6cbc0
juliasjy <juliasjy129@gmail.com>
2016-12-11 19:52:56 -0500

    Add files via upload

--- HW1.py
+++ HW1.py
@@ -3,25 +3,24 @@ import subprocess
 
 def main():
     argv = sys.argv
-    # argv = str(' -f 1234567 -s Note')
-    shaRepo = argv[4: 11]
-    stringForSearch = argv[15: ]
-    print(shaRepo)
-    print(stringForSearch)
+    shaRepo = argv[2]
+    stringForSearch = argv[4]
+    searchTree(shaRepo, stringForSearch)
 
-    SHAs = []
-    cmd = 'git cat-file -p ' + shaRepo
+def searchTree(sha, stringForSearch):
+    cmd = 'git cat-file -p ' + sha
     output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    for line in output.stdout.readline():
-        print(line)
-        SHAs.append(line[12:30])
+    for line in output.stdout.readlines():
+        strs = line.split()
+        SHA = strs[2]
+        if strs[1] == 'blob':
+            cmd1 = 'git cat-file -p ' + SHA + ' | grep ' + stringForSearch
+            output1 = subprocess.Popen(cmd1, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            s = output1.stdout.read();
+            if len(s) > 0 :
+                print s,
+            elif strs[1] == 'tree':
+                searchTree(SHA, stringForSearch)
 
-    for SHA in SHAs:
-        if SHA[7: 10] == 'blob':
-            cmd = 'git cat-file -p ' + SHA + ' | grep ' + stringForSearch
-            output = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            print(output.stdout.read())
-        else:
-            main()
-
-
+if __name__ == "__main__":
+        main()
