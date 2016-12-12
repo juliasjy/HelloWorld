ebcc7d011785c3f555dad7ae2eb6022645c0c58b
juliasjy <juliasjy129@gmail.com>
2016-12-11 19:53:02 -0500

    Add files via upload

---
+++ HW2.py
@@ -0,0 +1,40 @@
+"""
+<describe what this module has/does>
+
+Created on Dec 11, 2016.
+Written by: suj1.
+"""
+import argparse
+import subprocess
+
+def main():
+    parser = argparse.ArgumentParser();
+    parser.add_argument('-t', '--treeSHAs', help='Get a series of trees', action='store_true')
+    parser.add_argument('-o', '--output', help='Get the sha of output', action='store_true')
+    args = parser.parse_args();
+    parentSHA = commitTree(args)
+    updateRef(args, parentSHA)
+
+def commitTree(args):
+    parentSHA = None
+    for treeSHA in args.treeSHAs:
+        if parentSHA == None:
+            cmd = 'git commit-tree ' + treeSHA + '-m alternative begin'
+            output = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            parentSHA = output.stdout.readline().split()[1]
+        else:
+            cmd = 'git commit-tree ' + treeSHA + '-p' + parentSHA + '-m alternative begin'
+            output = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+            parentSHA = output.stdout.readline().split()[1]
+    return parentSHA
+
+def updateRef(args, parentSHA):
+    cmd = 'git update-ref refs/heads/' + args.output + ' ' + parentSHA
+    output = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
+#-----------------------------------------------------------------------
+# If this module is running at the top level (as opposed to being
+# imported by another module), then call the 'main' function.
+#-----------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
